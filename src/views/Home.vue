8df343eb9ce367ffb0d9bd0573a33ee43cdf2ee1
BALDE ALFA <alfaafro@gmail.com>
2021-03-30 18:15:17 +0000

    Setup

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png" />
+		<div class="counter">0</div>
+		<div class="buttons">
+			<button>-</button>
+			<button>+</button>
+		</div>
+	</div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+	name: "Home",
+};
+</script>
